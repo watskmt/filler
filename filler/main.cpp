913c82536f6ed6414f75b96faa5c676f17bd9b0e
LAPTOP-JUUUUUUU\PC-YUZUKI <watari-yuzuki@amami-joho.ac.jp>
2026-06-22 10:20:24 +0900

    作業途中

--- filler/main.cpp
+++ filler/main.cpp
@@ -18,6 +18,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	{
 		ClearDrawScreen();
 		DrawBox(100, 100, 199, 199, GetColor(255, 255, 255), FALSE); // 300x300の四角の枠を描画
+		fill(100, 100, GetColor(255, 255, 255));
 		ScreenFlip();
 	}
 
@@ -26,4 +27,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	return 0;
 }
 
-int fill(int x, int y, unsigned int color) { return 0; }
+int fill(int x, int y, unsigned int color) {
+	for (x; x < 199; x++) {
+		for (y; y < 199; y++) {
+			DrawPixel(x, y, color);
+		}
+	}
+
+	return 0;
+}
+
+//int fill1(int x, int y, unsigned int color) {
+//	// ここに塗りつぶしロジックを作成
+//
+//	// 例
+//	DrawPixel1(x, y, color);
+//	while (!outofBounds1(x + 1, y))
+//	{
+//		DrawPixel1(x++, y, color);
+//		DrawCells();
+//	}
+//
+//	return 0;
+//}
