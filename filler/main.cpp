400bbd62fbf3a53ed09ab44fc0648cc957ccfe17
R7600\watsk <watskmt@gmail.com>
2026-06-22 09:26:40 +0900

    テンプレートを更新（ペイント進捗がわかりやすいように変更）

--- filler/main.cpp
+++ filler/main.cpp
@@ -1,10 +1,21 @@
 #include "DxLib.h"
+#define XSIZE 25
+#define YSIZE 25
+#define WINDOW_WIDTH 400
+#define WINDOW_HEIGHT 400
 
-int fill(int x, int y, unsigned int color);
+
+int cells[YSIZE][XSIZE] = { 0 }; // セルを初期化
+
+int outofBounds1(int x, int y);
+unsigned int GetPixel1(int x, int y);
+void DrawPixel1(int x, int y, unsigned int color);
+int fill1(int x, int y, unsigned int color);
+void DrawCells();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	SetGraphMode(300, 300, 32);      // 画面サイズを300x300に設定
+	SetGraphMode(WINDOW_WIDTH, WINDOW_HEIGHT, 32);      // 画面サイズをWINDOW_WIDTHxWINDOW_HEIGHTに設定
 
 	if (DxLib_Init() == -1)
 	{
@@ -12,18 +23,42 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 	ChangeWindowMode(TRUE);          // ウィンドウモードに設定（フルスクリーンにしない）
 
-	SetDrawScreen(DX_SCREEN_BACK);
+	ClearDrawScreen();
 
-	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
-	{
-		ClearDrawScreen();
-		DrawBox(100, 100, 199, 199, GetColor(255, 255, 255), FALSE); // 300x300の四角の枠を描画
-		ScreenFlip();
-	}
+	fill1(15, 15, GetColor(255, 0, 0)); // (150, 150)を起点に赤色で塗りつぶす
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0);
 
 	DxLib_End();
 
 	return 0;
+} 
+
+int outofBounds1(int x, int y) {
+	if (x < 0 || x >= XSIZE || y < 0 || y >= YSIZE) return 1; // 範囲外の場合は1を返す
+	return 0; // 範囲内の場合は0を返す
 }
 
-int fill(int x, int y, unsigned int color) { return 0; }
+
+
+int fill1(int x, int y, unsigned int color) {
+	return 0;
+}
+
+unsigned int GetPixel1(int x, int y) {
+	if (x < 0 || x >= XSIZE || y < 0 || y >= YSIZE) return -1; // 範囲外の場合は-1を返す
+	return cells[y][x]; // セルの値を返す
+}
+
+void DrawPixel1(int x, int y, unsigned int color) {
+	if (x < 0 || x >= XSIZE || y < 0 || y >= YSIZE) return; // 範囲外の場合は何もしない
+	cells[y][x] = color; // セルに値を設定
+}
+
+void DrawCells() {
+	for (int y = 0; y < YSIZE; y++) {
+		for (int x = 0; x < XSIZE; x++) {
+			DrawBox(x * (WINDOW_WIDTH / XSIZE), y * (WINDOW_HEIGHT / YSIZE), (x + 1) * (WINDOW_WIDTH / XSIZE) - 1, (y + 1) * (WINDOW_HEIGHT / YSIZE) - 1, cells[y][x], TRUE); // セルの値を描画
+		}
+	}
+	WaitTimer(100);
+}
