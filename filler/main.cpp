ed5891c2894ec8315552e711ec98d43623a79c81
LAPTOP-GR49S8PR\PC-HIBIKI <katsuhisa-hibiki@amami-joho.ac.jp>
2026-06-22 10:10:20 +0900

    枠の中でクリックしたら色を塗る

--- filler/main.cpp
+++ filler/main.cpp
@@ -1,25 +1,42 @@
 #include "DxLib.h"
 
+int mx, my;
+int fill(int x, int y, unsigned int cr = GetColor(255, 255, 0));
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	SetGraphMode(300, 300, 32);      // 画面サイズを300x300に設定
+	SetGraphMode(300, 300, 32);      
 
 	if (DxLib_Init() == -1)
 	{
 		return -1;
 	}
-	ChangeWindowMode(TRUE);          // ウィンドウモードに設定（フルスクリーンにしない）
-
+	ChangeWindowMode(TRUE);          
+	SetMouseDispFlag(TRUE);
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
-		ClearDrawScreen();
-		DrawBox(100, 100, 199, 199, GetColor(255, 255, 255), FALSE); // 300x300の四角の枠を描画
+		//ClearDrawScreen();
 		ScreenFlip();
+		DrawBox(100, 100, 199, 199, GetColor(255, 255, 255), FALSE);
+		
+		if (GetMouseInput() & MOUSE_INPUT_LEFT){
+			GetMousePoint(&mx, &my);// マウスカーソルの座標を取得
+			//マウスカーソルが枠線の中かどうかの判定
+			if (mx >= 100 && mx <= 199 &&my >= 100 && my <= 199){
+				fill(mx, my);// クリックした位置を塗りつぶし
+			}
+		}
+		
 	}
-
+	 
+	
 	DxLib_End();
 
+	return 0;
+}
+int fill(int x, int y, unsigned int cr){
+	DrawPixel(x, y, cr);
 	return 0;
 }
